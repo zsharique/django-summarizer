2b6279bf2d9f1b78ecd18fbb945777b1d30965ee
zsharique <shariquezia94@gmail.com>
2016-04-01 23:56:18 +0530

    add upload file

--- app/forms.py
+++ app/forms.py
@@ -2,3 +2,6 @@ from django import forms
 
 class Paragraph(forms.Form):
     Text = forms.CharField(widget = forms.Textarea)
+
+class UploadFileForm(forms.Form):
+	Upload = forms.FileField()    
--- app/views.py
+++ app/views.py
@@ -1,18 +1,33 @@
 from django.shortcuts import render,render_to_response
 from django.template.loader import get_template
 from django.template import Context, RequestContext
-from .forms import Paragraph
+from .forms import Paragraph, UploadFileForm
 from . import getSummary
 
 
 def landing_view(request):
 	result = []
-	form = Paragraph(request.POST or None)
-	if form.is_valid():
-		text = form.cleaned_data.get('Text')
-		result = getSummary.main(text)
+	form1 = Paragraph(request.POST or None)
+	form2 = UploadFileForm(request.POST, request.FILES)
+	if form1.is_valid():
+		result = getSummary.main(form1.cleaned_data.get('Text'))
+		form1 = Paragraph()
+		form2 = UploadFileForm()
+	elif form2.is_valid():
+		result = getSummary.main(handle_file(request.FILES['Upload']))
+		form1 = Paragraph()
+	else:
+		form1 = Paragraph()
+		form2 = UploadFileForm()
 	context = {
-		"form" : form,
+		"form1" : form1,
+		"form2" : form2,
 		"result" : result,
 	}
 	return render(request, "landing.html", context)
+
+def handle_file(f):
+	f.seek(0)
+	para = f.read()
+	para = para.replace('\n',' ')
+	return para	
